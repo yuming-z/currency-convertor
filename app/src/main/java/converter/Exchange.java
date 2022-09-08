b82e770d8553f145e23837334bb1902dc228675f
Yuming Zhu <yzhu6876@uni.sydney.edu.au>
2022-09-08 21:10:18 +1000

    Read username from stdin and validation mechanism

--- app/src/main/java/converter/Exchange.java
+++ app/src/main/java/converter/Exchange.java
@@ -3,10 +3,11 @@
  */
 package converter;
 
-import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Currency;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 public class Exchange {
     
@@ -15,7 +16,14 @@ public class Exchange {
     private HashMap<Currency, HashMap<Currency, Double>> rates;
     private Currency[] popularCurrencies;
 
-    public User getUser(String username) throws FileNotFoundException {
+    public Exchange() {
+        this.users = new ArrayList<User>();
+        this.currencies = new ArrayList<Currency>();
+        this.rates = new HashMap<Currency, HashMap<Currency, Double>>();
+        this.popularCurrencies = new Currency[4];
+    }
+
+    public User getUser(String username) {
 
         for (int i = 0; i < users.size(); i++) {
 
@@ -24,6 +32,44 @@ public class Exchange {
             }
         }
 
-        throw new FileNotFoundException("There is no such user in the system");
+        return null;
+    }
+
+    public static void main(String[] args) {
+        
+        // Welcome message
+        Exchange market = new Exchange();
+        System.out.println("Welcome to use the currency converter.");
+
+        // Get username from stdin
+        String username;
+        User user;
+        final int ATTEMPTS = 3; // number of attempts that the user is allowed to enter username
+
+        System.out.println("Please enter your username:");
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 0; i < ATTEMPTS; i++) {
+            username = scan.nextLine();
+            user = market.getUser(username);
+
+            if (user != null) {
+                break;
+            }
+
+            if (i == ATTEMPTS - 1 && user == null) {
+                // if it is the last attempt and still no user can be found
+                // print error message and terminate the system
+                System.err.println("You are failing too many times.");
+                System.out.println("System terminating...");
+                return;
+            }
+            else {
+                // if the user does not exist
+                // print the error message and show the number of attempts left
+                System.err.println("Wrong username. Please try again.");
+                System.out.println(String.format("You have %d attempts left.", ATTEMPTS - (i + 1)));
+            }
+        }
     }
 }
