0d683717f5b8d6a75a252b30a3615970b3e35e55
Alisha Rashid <anrashid20@MacBook-Air.local>
2022-09-09 16:43:55 +1000

    add daily currency added

--- app/src/main/java/converter/AdminUser.java
+++ app/src/main/java/converter/AdminUser.java
@@ -1,11 +1,20 @@
 package converter;
 
-import java.util.HashMap;
+import java.util.*;
 import java.util.Map;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.io.FileWriter;
+import java.io.IOException;
+
 
 public class AdminUser implements User{
 
     private Exchange exchange;
+
+    private String date;
     private Map<String, HashMap<String, String>> currency = new HashMap<String, HashMap<String, String>>();
     //6 currencies
     public AdminUser(Exchange exchange){
@@ -17,6 +26,39 @@ public class AdminUser implements User{
         this.currency = exchange.getHashMap();
         return true;
     }
+
+    public boolean addCurrencyDaily(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Whats the date today? in form 00/00/00");
+        String date = scan.nextLine();
+        if (date == this.date){
+            System.out.println("Currencies have already been entered for today");
+        }
+        JSONObject rates = new JSONObject();
+        rates.put("date", date);
+
+        for(int i = 1;  i <= 6;  i++) {
+            System.out.println("" + i + " currency to alter rates of is?(use symbol)");
+            String currency = scan.nextLine();
+            rates.put("currency", currency);
+            System.out.println("First currency and rate exchange (e.g AUS 0.000):");
+            String placeAndrate1 = scan.nextLine();
+            String[] symbolAndrate1 = placeAndrate1.split(" ");
+            rates.put(symbolAndrate1[1], symbolAndrate1[2]);
+        }
+        System.out.println("Limit of currencies have been reached.");
+        try (FileWriter file = new FileWriter("config.json")) {
+            file.write(rates.toJSONString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return true;
+
+    }
+
+
 
     @Override
     public double convertCurrency(double amount, String currency1, String currency2) {
