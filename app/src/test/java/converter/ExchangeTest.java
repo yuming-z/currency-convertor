becc512725c22a9fa655f8790b37d6052d41571f
Yuming Zhu <yzhu6876@uni.sydney.edu.au>
2022-09-14 14:18:44 +1000

    Test normal database loading

--- app/src/test/java/converter/ExchangeTest.java
+++ app/src/test/java/converter/ExchangeTest.java
@@ -3,21 +3,44 @@ package converter;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.util.Scanner;
+import java.util.Currency;
 
 class ExchangeTest {
     
     @Test
-    void testNoUser() {
-        Exchange market = new Exchange();
-        
-        // Simulate user input
-        String input = "hello\nhello\nhello\n";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
+    void testDatabaseLoad() {
+        Exchange market = new Exchange("src/test/resources/config_correct.json");
 
-        Scanner scan = new Scanner(in);
-        assertNull(market.getUser(scan), "There is no user in the system.");
+        int numberOfCurrencies = 6;
+
+        // test whether the database is correctly loaded
+        assertTrue(
+            market.refreshDatabase(),
+            "The database should be able to be loaded");
+        
+        // test currency collection
+        assertNotNull(
+            market.getCurrencies(),
+            "There should be contents in the currency coolection");
+        assertEquals(
+            numberOfCurrencies,
+            market.getCurrencies().size(),
+            "There should be 6 currencies loaded in the system");
+        
+        // test rates collection
+        assertNotNull(
+            market.getRates(),
+            "There should be some contents in the exchange rates collection");
+        assertEquals(
+            numberOfCurrencies,
+            market.getRates().size(),
+            "There should be a total of 6 exchange rates record");
+        
+        for (Currency currency: market.getRates().keySet()) {
+            assertEquals(
+                numberOfCurrencies - 1,
+                market.getRates().get(currency).size(),
+                "There should be 5 records inside for each currency");
+        }
     }
 }
