72e6bbc7987256ae54047d56fd341de8fb9b17d1
Yuming Zhu <yzhu6876@uni.sydney.edu.au>
2022-09-14 13:22:22 +1000

    Configure exchange rates database loading

--- app/src/main/java/converter/Exchange.java
+++ app/src/main/java/converter/Exchange.java
@@ -1,25 +1,33 @@
 package converter;
 
+import java.io.FileReader;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Currency;
 import java.util.HashMap;
 import java.util.List;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 public class Exchange {
     
     private List<User> users;
     private List<Currency> currencies;
-    private HashMap<Currency, HashMap<Currency, Double>> rates;
+    private HashMap<Currency, HashMap<Currency, Double>> rates; // always store the latest rate
     private Currency[] popularCurrencies;
 
     private final int ATTEMPTS; // number of attempts that the user is allowed to enter username
+    private final String DATABASE_PATH;
 
-    public Exchange() {
+    public Exchange(String path) {
         this.users = new ArrayList<User>();
         this.currencies = new ArrayList<Currency>();
         this.rates = new HashMap<Currency, HashMap<Currency, Double>>();
         this.popularCurrencies = new Currency[4];
         this.ATTEMPTS = 3;
+        this.DATABASE_PATH = path;
     }
 
     public void terminate() {
@@ -103,10 +111,75 @@ public class Exchange {
         return null;
     }
 
+    /**
+     * Read exchange rate from JSON file
+     * @param jsonObject JSON object storing the rates 
+     * @param target the currency to be converted to
+     * @param base the currency to be converted from
+     */
+    private HashMap<Currency, Double> loadRate(JSONObject jsonObject, Currency target) {
+
+        HashMap<Currency, Double> rate = new HashMap<Currency, Double>();
+
+        String key = target.toString();
+        double value = Double.parseDouble(jsonObject.get(key).toString());
+
+        rate.put(target, value);
+        return rate;
+    }
+    
+    private Currency loadCurrency(JSONObject currency) {
+
+        String currencyCode = (String)currency.get("currency");
+        return Currency.getInstance(currencyCode);
+    }
+
+    public boolean refreshDatabase() {
+        JSONParser parser = new JSONParser();
+
+        try {
+            // Read the file
+            FileReader reader = new FileReader(this.DATABASE_PATH);
+
+            // convert the JSON file to JSONArray
+            JSONArray database = (JSONArray)parser.parse(reader);
+
+            // load currencies
+            for (int i = 0; i < database.size(); i++) {
+                this.currencies.add(loadCurrency((JSONObject)database.get(i)));
+            }
+
+            // load latest rate
+            for (int i = 0; i < database.size(); i++) {
+                JSONObject currency = (JSONObject)database.get(i);
+                
+                Currency base = loadCurrency(currency);
+                JSONArray rates = (JSONArray)currency.get("rates");
+                JSONObject latestRate = (JSONObject)rates.get(rates.size() - 1);
+
+                for (Currency target: this.currencies) {
+                    if (base.equals(target)) {
+                        continue;
+                    }
+
+                    this.rates.put(
+                        base,
+                        loadRate(latestRate, target));
+                }
+            }
+
+            reader.close();
+            return true;
+        } catch (Exception e) {
+            System.err.println("The exchange database cannot be loaded.");
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
      
         // Welcome message
-        Exchange market = new Exchange();
+        Exchange market = new Exchange("src/main/resources/config.json");
         System.out.println("Welcome to use the currency converter.");
 
         // User login interface
